f5d3287cc609d1699b4beab826d28ce0f8576996
Promise Morka <onyekamorka06@gmail.com>
2022-05-07 09:14:19 -0400

    Initial commit

---
+++ insertion.js
@@ -0,0 +1,19 @@
+function insertionSort(array) {
+	// Write your code here.
+	for (let i = 0; i < array.length; i++) {
+		let j = i;
+		while (j > 0 && array[j] < array[j - 1]) {
+			swap(j, j - 1, array);
+			j -= 1;
+		}
+	}
+	return array;
+}
+
+function swap(i, j, array) {
+	const temp = array[j];
+	array[j] = array[i];
+	array[i] = temp;
+}
+
+module.exports = insertionSort;
